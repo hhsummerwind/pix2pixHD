37c8c4657bd70f7f9a247d89b00fa73285311a44
huhui <1003369585@qq.com>
2019-12-24 00:01:44 +0800

    modify dataset path, train full cityscapes

--- data/aligned_dataset_ori.py
+++ data/aligned_dataset_ori.py
@@ -4,45 +4,40 @@ import os.path
 from data.base_dataset import BaseDataset, get_params, get_transform, normalize
 from data.image_folder import make_dataset
 from PIL import Image
-import glob
-
 
 class AlignedDataset(BaseDataset):
     def initialize(self, opt):
         self.opt = opt
-        self.root = opt.dataroot
+        self.root = opt.dataroot    
 
         ### input A (label maps)
-        # dir_A = '_A' if self.opt.label_nc == 0 else '_label'
-        # self.dir_A = os.path.join(opt.dataroot, opt.phase + dir_A)
-        # self.A_paths = sorted(make_dataset(self.dir_A))
-        self.A_paths = sorted(glob.glob(os.path.join(self.opt.dataroot, 'gtFine', self.opt.phase, '*', '*_gtFine_labelIds.png')))
+        dir_A = '_A' if self.opt.label_nc == 0 else '_label'
+        self.dir_A = os.path.join(opt.dataroot, opt.phase + dir_A)
+        self.A_paths = sorted(make_dataset(self.dir_A))
 
         ### input B (real images)
         if opt.isTrain:
-            # dir_B = '_B' if self.opt.label_nc == 0 else '_img'
-            # self.dir_B = os.path.join(opt.dataroot, opt.phase + dir_B)
-            # self.B_paths = sorted(make_dataset(self.dir_B))
-            self.B_paths = sorted(glob.glob(os.path.join(self.opt.dataroot, 'leftImg8bit', self.opt.phase, '*', '*_leftImg8bit.png')))
+            dir_B = '_B' if self.opt.label_nc == 0 else '_img'
+            self.dir_B = os.path.join(opt.dataroot, opt.phase + dir_B)  
+            self.B_paths = sorted(make_dataset(self.dir_B))
 
-            ### instance maps
+        ### instance maps
         if not opt.no_instance:
-            # self.dir_inst = os.path.join(opt.dataroot, opt.phase + '_inst')
-            # self.inst_paths = sorted(make_dataset(self.dir_inst))
-            self.inst_paths = sorted(glob.glob(os.path.join(self.opt.dataroot, 'gtFine', self.opt.phase, '*', '*_gtFine_instanceIds.png')))
+            self.dir_inst = os.path.join(opt.dataroot, opt.phase + '_inst')
+            self.inst_paths = sorted(make_dataset(self.dir_inst))
 
-            ### load precomputed instance-wise encoded features
-        if opt.load_features:
+        ### load precomputed instance-wise encoded features
+        if opt.load_features:                              
             self.dir_feat = os.path.join(opt.dataroot, opt.phase + '_feat')
             print('----------- loading features from %s ----------' % self.dir_feat)
             self.feat_paths = sorted(make_dataset(self.dir_feat))
 
-        self.dataset_size = len(self.A_paths)
-
-    def __getitem__(self, index):
+        self.dataset_size = len(self.A_paths) 
+      
+    def __getitem__(self, index):        
         ### input A (label maps)
-        A_path = self.A_paths[index]
-        A = Image.open(A_path)
+        A_path = self.A_paths[index]              
+        A = Image.open(A_path)        
         params = get_params(self.opt, A.size)
         if self.opt.label_nc == 0:
             transform_A = get_transform(self.opt, params)
@@ -54,9 +49,9 @@ class AlignedDataset(BaseDataset):
         B_tensor = inst_tensor = feat_tensor = 0
         ### input B (real images)
         if self.opt.isTrain:
-            B_path = self.B_paths[index]
+            B_path = self.B_paths[index]   
             B = Image.open(B_path).convert('RGB')
-            transform_B = get_transform(self.opt, params)
+            transform_B = get_transform(self.opt, params)      
             B_tensor = transform_B(B)
 
         ### if using instance maps        
@@ -66,12 +61,12 @@ class AlignedDataset(BaseDataset):
             inst_tensor = transform_A(inst)
 
             if self.opt.load_features:
-                feat_path = self.feat_paths[index]
+                feat_path = self.feat_paths[index]            
                 feat = Image.open(feat_path).convert('RGB')
                 norm = normalize()
-                feat_tensor = norm(transform_A(feat))
+                feat_tensor = norm(transform_A(feat))                            
 
-        input_dict = {'label': A_tensor, 'inst': inst_tensor, 'image': B_tensor,
+        input_dict = {'label': A_tensor, 'inst': inst_tensor, 'image': B_tensor, 
                       'feat': feat_tensor, 'path': A_path}
 
         return input_dict
--- models/networks.py
+++ models/networks.py
@@ -114,7 +114,8 @@ class GANLoss(nn.Module):
 class VGGLoss(nn.Module):
     def __init__(self, gpu_ids):
         super(VGGLoss, self).__init__()        
-        self.vgg = Vgg19().cuda()
+        # self.vgg = Vgg19().cuda()
+        self.vgg = Vgg16().cuda()
         self.criterion = nn.L1Loss()
         self.weights = [1.0/32, 1.0/16, 1.0/8, 1.0/4, 1.0]        
 
@@ -414,5 +415,38 @@ class Vgg19(torch.nn.Module):
         h_relu3 = self.slice3(h_relu2)        
         h_relu4 = self.slice4(h_relu3)        
         h_relu5 = self.slice5(h_relu4)                
+        out = [h_relu1, h_relu2, h_relu3, h_relu4, h_relu5]
+        return out
+
+
+class Vgg16(torch.nn.Module):
+    def __init__(self, requires_grad=False):
+        super(Vgg16, self).__init__()
+        vgg_pretrained_features = models.vgg16(pretrained=True).features
+        self.slice1 = torch.nn.Sequential()
+        self.slice2 = torch.nn.Sequential()
+        self.slice3 = torch.nn.Sequential()
+        self.slice4 = torch.nn.Sequential()
+        self.slice5 = torch.nn.Sequential()
+        for x in range(2):
+            self.slice1.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(2, 7):
+            self.slice2.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(7, 12):
+            self.slice3.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(12, 21):
+            self.slice4.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(21, 30):
+            self.slice5.add_module(str(x), vgg_pretrained_features[x])
+        if not requires_grad:
+            for param in self.parameters():
+                param.requires_grad = False
+
+    def forward(self, X):
+        h_relu1 = self.slice1(X)
+        h_relu2 = self.slice2(h_relu1)
+        h_relu3 = self.slice3(h_relu2)
+        h_relu4 = self.slice4(h_relu3)
+        h_relu5 = self.slice5(h_relu4)
         out = [h_relu1, h_relu2, h_relu3, h_relu4, h_relu5]
         return out
